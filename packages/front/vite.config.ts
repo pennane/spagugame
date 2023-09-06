0c77155334a85cfd112df98dfae2cbf492b50b04
Arttu Pennanen <arttu@pennanen.org>
2023-09-06 13:54:13 +0300

    chore(front): revert & update rewrite options

--- packages/front/vite.config.ts
+++ packages/front/vite.config.ts
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/': {
-        target: '',
-        secure: false,
-        ws: false,
-        rewrite: (path) => (path === '/' ? path : '/index.html')
-      }
-    }
-  }
+  plugins: [react()]
 })
